ba3fd6a592dd607cb9b3cb16f33ffa18457e94ca
Gagandeep <igagandeep95@gmail.com>
2023-04-08 00:48:49 -0400

    added form validations

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,125 @@
 // TOGGLE MENU
-
 const hamburger = document.querySelector(".hamburger");
 const navMenuBar = document.querySelector(".nav");
 const shoppingBag = document.querySelector(".shopping-bag");
 hamburger.addEventListener("click", () => {
   navMenuBar.classList.toggle("nav-sm");
   shoppingBag.classList.toggle("nav-sm");
+});
+
+//Select inputs from query selector and store in respective variables.
+const formElement = document.querySelector("form");
+const firstNameElement = document.querySelector("#firstName");
+const lastNameElement = document.querySelector("#lastName");
+const phoneElement = document.querySelector("#phone");
+const emailElement = document.querySelector("#email");
+const messageElement = document.querySelector("#message");
+const submitButtonElement = document.querySelector("#submit");
+
+// Disable the submit button initially
+submitButtonElement.disabled = true;
+
+// add event listener on every element
+firstNameElement.addEventListener("blur", validateForm);
+lastNameElement.addEventListener("blur", validateForm);
+phoneElement.addEventListener("blur", validateForm);
+emailElement.addEventListener("blur", validateForm);
+messageElement.addEventListener("blur", validateForm);
+
+// these variables check validity of every input element and return boolean
+let isFirstNameValid,
+  islastNameValid,
+  isPhoneValid,
+  isEmailValid,
+  isMessageValid = false;
+
+// callback fn to validate every input element
+function validateForm(e) {
+  const inputElement = e.target;
+
+  if (inputElement.id === "firstName") {
+    if (inputElement.value === "") {
+      errorMessages(inputElement, "First Name is required");
+      isFirstNameValid = false;
+    } else {
+      errorMessages(inputElement);
+      isFirstNameValid = true;
+    }
+    console.log(inputElement.value);
+  }
+  if (inputElement.id === "lastName") {
+    if (inputElement.value === "") {
+      errorMessages(inputElement, "Last Name is required");
+      islastNameValid = false;
+    } else {
+      errorMessages(inputElement);
+      islastNameValid = true;
+    }
+  }
+  if (inputElement.id === "phone") {
+    const phoneFormat = /^[0-9]{10}$/;
+    if (phoneElement.value === "") {
+      errorMessages(phoneElement, "Phone no. is required");
+      isPhoneValid = false;
+    } else if (!phoneFormat.test(phoneElement.value)) {
+      errorMessages(phoneElement, "Phone no. is not valid");
+      isPhoneValid = false;
+    } else {
+      errorMessages(phoneElement);
+      isPhoneValid = true;
+    }
+  }
+  if (inputElement.id === "email") {
+    if (emailElement.value === "") {
+      errorMessages(emailElement, "Email is required");
+      isEmailValid = false;
+    } else if (!emailElement.checkValidity()) {
+      errorMessages(emailElement, "Please enter a valid email");
+      isEmailValid = false;
+    } else {
+      errorMessages(emailElement);
+      isEmailValid = true;
+    }
+  }
+  if (inputElement.id === "message") {
+    if (messageElement.value === "") {
+      errorMessages(messageElement, "Message is required");
+      isMessageValid = false;
+    } else {
+      errorMessages(messageElement);
+      isMessageValid = true;
+    }
+  }
+  // this condition check validity of every input element and enable or diable the submit btn
+  if (
+    isFirstNameValid &&
+    islastNameValid &&
+    isPhoneValid &&
+    isEmailValid &&
+    isMessageValid
+  ) {
+    submitButtonElement.disabled = false;
+  } else {
+    submitButtonElement.disabled = true;
+  }
+}
+
+// This fn will recieve the args and render the msg to the page
+const errorMessages = (inputElement, errorMsg = null) => {
+  //   chekcing if there is any error msg
+  if (errorMsg) {
+    // this will make span element display and add the error msg
+    inputElement.nextElementSibling.style.display = "inline";
+    inputElement.style.borderBottom = "1px solid #ff3333";
+    inputElement.nextElementSibling.textContent = errorMsg;
+  } else {
+    // if there is no error msg, hide the span element
+    inputElement.nextElementSibling.style.display = "none";
+    inputElement.style.borderBottom = "1px solid  lightgrey";
+  }
+};
+
+// add event listener on submit form
+formElement.addEventListener("submit", () => {
+  alert("Message has sent successfully");
 });
