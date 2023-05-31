6411d0c6e6c83664e1948cc2c62bb0abffbd9317
Gagandeep <igagandeep95@gmail.com>
2023-05-31 16:15:24 -0400

    called toggleCart and toggleMenu funcs 💥💥

--- scripts/script.js
+++ scripts/script.js
@@ -1,76 +1,81 @@
 import { toggleMenu, toggleCart } from "./toggleFun.js";
 import {
-  ref,
-  get,
-  onValue,
+    ref,
+    get,
+    onValue,
 } from "https://www.gstatic.com/firebasejs/9.19.1/firebase-database.js";
 import {
-  productsInventoryRef,
-  productsContainer,
-  displayItems,
+    productsInventoryRef,
+    productsContainer,
+    displayItems,
 } from "./products.js";
 
 import {
-  database,
-  cartRef,
-  addToCart,
-  cartInfoElement,
-  displayCart,
-  displayTotalSize,
-  emptyCart,
-  updateItemFromCart,
-  removeProductFromCart,
+    database,
+    cartRef,
+    addToCart,
+    cartInfoElement,
+    displayCart,
+    displayTotalSize,
+    emptyCart,
+    updateItemFromCart,
+    removeProductFromCart,
 } from "./cart.js";
+
+toggleCart();
+toggleMenu();
 
 // using onvalue to display list of products on page and populate those on every change
 onValue(productsInventoryRef, (snapshot) => {
-  const productsInventory = snapshot.val();
-  displayItems(productsInventory);
+    const productsInventory = snapshot.val();
+    displayItems(productsInventory);
 });
 
 // Adding products in to cart and updating stock in products inventory
 productsContainer.addEventListener("click", (e) => {
-  // if targeting element id is equal to cart then do something
-  if (e.target.id === "cart") {
-    addToCart(e.target.parentElement.id);
-  }
+    // if targeting element id is equal to cart then do something
+    if (e.target.id === "cart") {
+        addToCart(e.target.parentElement.id);
+    }
 });
 
 // Showing the list of items on cart widget and populate those on every change
 onValue(cartRef, (snapshot) => {
-  const cartItems = snapshot.val();
-  displayTotalSize(cartItems);
-  if (cartItems) {
-    displayCart(cartItems);
-  } else {
-    emptyCart();
-  }
+    const cartItems = snapshot.val();
+    displayTotalSize(cartItems);
+    if (cartItems) {
+        displayCart(cartItems);
+    } else {
+        emptyCart();
+    }
 });
 
 /* binding the event listener to cart info element which is the parent element of cart items and with the the bubbling effect,
 can modifiy the items present in the cart  */
 cartInfoElement.addEventListener("click", (e) => {
-  const key = e.target.parentElement.id;
-  const cartItemRef = ref(database, `/cart/${key}`);
-  get(cartItemRef).then((snapshot) => {
-    const cartItemSnapshot = snapshot.val();
-    if (cartItemSnapshot) {
-      if (e.target.className === "plus") {
-        if (cartItemSnapshot.quantityInCart < 10) {
-          // when user want to add the quantity of products
-          addToCart(key);
-        } else {
-          alert(`No more quantity left for ${cartItemSnapshot.title} `);
+    const key = e.target.parentElement.id;
+    const cartItemRef = ref(database, `/cart/${key}`);
+    get(cartItemRef).then((snapshot) => {
+        const cartItemSnapshot = snapshot.val();
+        if (cartItemSnapshot) {
+            if (e.target.className === "plus") {
+                if (cartItemSnapshot.quantityInCart < 10) {
+                    // when user want to add the quantity of products
+                    addToCart(key);
+                } else {
+                    alert(
+                        `No more quantity left for ${cartItemSnapshot.title} `
+                    );
+                }
+            }
+            if (e.target.className === "minus") {
+                // when user want to reduce the quantity of products
+                updateItemFromCart(key);
+            }
+            if (e.target.tagName === "I") {
+                removeProductFromCart(key);
+                alert("Removed item from cart");
+            }
         }
-      }
-      if (e.target.className === "minus") {
-        // when user want to reduce the quantity of products
-        updateItemFromCart(key);
-      }
-      if (e.target.tagName === "I") {
-        removeProductFromCart(key);
-        alert("Removed item from cart");
-      }
-    }
-  });
+    });
 });
--- scripts/toggleFun.js
+++ scripts/toggleFun.js
@@ -7,18 +7,18 @@ const shoppingCartIcon = document.querySelector(".shopping-bag img");
 const shoppingCartItems = document.querySelector(".cart-design-container");
 
 export const toggleMenu = hamburger.addEventListener("click", () => {
-  navMenuBar.classList.toggle("nav-sm");
-  shoppingBag.classList.toggle("nav-sm");
+    navMenuBar.classList.toggle("nav-sm");
+    shoppingBag.classList.toggle("nav-sm");
 });
 
 // Toggle Cart Widget
-export const toggleCart = shoppingCartIcon.addEventListener("click", (e) => {
-  if (
-    shoppingCartItems.style.display == "" ||
-    shoppingCartItems.style.display == "none"
-  ) {
-    shoppingCartItems.style.display = "block";
-  } else {
-    shoppingCartItems.style.display = "none";
-  }
+export const toggleCart = shoppingCartIcon.addEventListener("click", () => {
+    if (
+        shoppingCartItems.style.display == "" ||
+        shoppingCartItems.style.display == "none"
+    ) {
+        shoppingCartItems.style.display = "block";
+    } else {
+        shoppingCartItems.style.display = "none";
+    }
 });
